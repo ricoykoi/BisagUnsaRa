0823f1781778239725e19c97eaadc461a4251fca
Cmhanz99 <hanzhmagbal@gmail.com>
2025-12-08 19:20:59 +0800

    added furfur mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import cors from "cors";
+import connectDB from "./config/db.js";
 
 const app = express();
+connectDB();
 
 app.use(express.json());
 app.use(cors());
