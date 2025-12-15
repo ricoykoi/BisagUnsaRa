901319513ad4cc11b525e6282138a49b40b98ef4
ricoykoi <ricoykoi@gmail.com>
2025-12-15 15:17:44 +0800

    .

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(error.message);
