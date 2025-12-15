7b7a181642cc4858fc8554cbb904b074789a60da
ricoykoi <ricoykoi@gmail.com>
2025-12-15 14:59:12 +0800

    hemlo

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
 
+// Load environment variables from .env file
+import dotenv from 'dotenv';
+dotenv.config();
+
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://localhost:27017/furfur");
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(error.message);
