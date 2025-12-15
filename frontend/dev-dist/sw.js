e6e55aa66610d75975972bac92060a3ef7fb7886
Cmhanz99 <hanzhmagbal@gmail.com>
2025-12-15 20:06:20 +0800

    .

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-79fb5871'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.msssu8ggb2"
+    "revision": "0.s66rn5ldsv"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
