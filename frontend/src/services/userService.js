43afaa4e72b3d7f22b6c9bc5b75dc9faaf78abe2
ricoykoi <ricoykoi@gmail.com>
2025-12-15 14:26:37 +0800

    .

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api/auth";
+const API_URL = `${import.meta.env.VITE_PWA}/auth`;
 
 // REGISTER
 export const registerUser = async (userData) => {
